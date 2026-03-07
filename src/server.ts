b3b2067a603eaff08199751469cd43d5367e8371
OlenaKa <72217636+OlenaKa@users.noreply.github.com>
2026-03-07 14:59:37 +0100

    fixing db connection

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,8 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import routes from './routes';
 import { errorHandler, notFoundHandler } from './middleware/errorHandler';
-import { testConnection } from './config/database';
+// import { testConnection } from './config/database';
+import { testConnection } from './config/db';
 
 dotenv.config();
 
