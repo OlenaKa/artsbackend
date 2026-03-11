4f85fb7a59d1f39d1ab4569c1d453dd35dad2e83
OlenaKa <72217636+OlenaKa@users.noreply.github.com>
2026-03-11 16:39:44 +0100

    adding rollup prices endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,6 +8,7 @@ import { getPapirneNalepnicePricing } from '../controllers/papirneNalepniceContr
 import { getDigitalnaStampaPricing } from '../controllers/digitalnaStampaController';
 import { getMagneti2dPricing } from '../controllers/magneti2dController';
 import { getMagneti3dPricing } from '../controllers/magneti3dController';
+import { getRollupPricing } from '../controllers/rollupController';
 
 const router = Router();
 
@@ -20,5 +21,6 @@ router.get('/papirne-nalepnice-pricing', getPapirneNalepnicePricing);
 router.get('/digitalna-stampa-pricing', getDigitalnaStampaPricing);
 router.get('/magneti-2d-pricing', getMagneti2dPricing);
 router.get('/magneti-3d-pricing', getMagneti3dPricing);
+router.get('/rollup-pricing', getRollupPricing);
 
 export default router;
