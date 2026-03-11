7a735658423466e40ddcb20839a3f7d834c312dc
OlenaKa <72217636+OlenaKa@users.noreply.github.com>
2026-03-11 14:37:04 +0100

    making endpoint for zastitne nalepnice

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import { healthCheck } from '../controllers/healthController';
 import { getCategories } from '../controllers/categoriesController';
 import { getStikeriPricing } from '../controllers/stikeriController';
 import { getPvcNalepnicePrices } from '../controllers/pvcNalepniceController';
+import { getZastitneNalepnicePricing } from '../controllers/zastitneNalepniceController';
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.get('/health', healthCheck);
 router.get('/categories', getCategories);
 router.get('/stikeri', getStikeriPricing);
 router.get('/pvc-nalepnice-prices', getPvcNalepnicePrices);
+router.get('/zastitne-nalepnice-pricing', getZastitneNalepnicePricing);
 
 export default router;
